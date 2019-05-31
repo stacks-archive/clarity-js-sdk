65a0c6e970312c94e5ed897c91812b3b1b4c832d
Ludovic Galabru <ludovic@blockstack.com>
2019-05-31 14:46:25 -0400

    Transaction implements Submittable

--- packages/clarity/src/core/transaction.ts
+++ packages/clarity/src/core/transaction.ts
@@ -1,23 +1,42 @@
 import { Receipt, Method } from ".";
+import { Provider } from "./provider";
+import { Submittable } from "./submittable";
+import { Principal } from "./principal";
 
-export class Transaction {
-  sender?: string;
+export class Transaction implements Submittable {
+  contractPrincipal: string;
+  method: Method;
+  issuer: Principal;
+  provider?: Provider;
   receipt?: Receipt;
-  method?: Method;
 
-  constructor(method: Method) {
-    this.method = method;
+  constructor() {}
+
+  async sign(params: { issuer: Principal }): Promise<boolean> {
+    this.issuer = params.issuer;
+    return Promise.resolve(true);
   }
 
-  validate = async (): Promise<boolean> => {
-    if (!this.sender) {
-      throw Error("Transaction should be signed");
+  async submit(): Promise<Receipt> {
+    let receipt: Receipt;
+    try {
+      receipt = await this.provider.execute(
+        this.contractPrincipal,
+        this.method.name,
+        this.issuer.stacksAddress,
+        ...this.method.args
+      );
+    } catch (error) {
+      receipt = { success: false, error: error };
     }
-    return true;
-  };
+    return receipt;
+  }
 
-  sign = async (sender: string): Promise<boolean> => {
-    this.sender = sender;
-    return true;
-  };
+  async signAndSubmit(params: { issuer: Principal }): Promise<Receipt> {
+    const res = await this.sign(params);
+    if (!res) {
+      throw Error("Unable to sign transaction");
+    }
+    return await this.submit();
+  }
 }
